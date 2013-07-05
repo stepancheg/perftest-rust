a7303c6614bebcc73462850cf84b49e39d22c5ca
User <stepancheg@yandex-team.ru>
2013-07-05 18:25:53 +0400

    performance of time::get_time()
    74ns

---
+++ get_time.rs
@@ -0,0 +1,29 @@
+extern mod std;
+extern mod extra;
+
+use std::io;
+
+use extra::time;
+
+fn to_us(ts: &time::Timespec) -> u64 {
+    ts.sec * 1000000 as u64 + ((ts.nsec / 1000) as u64)
+}
+
+fn now_us() -> u64 {
+    to_us(&time::get_time())
+}
+
+// result is 74ns
+fn main() {
+    loop {
+        let start = now_us();
+        let mut count = 0;
+        while (now_us() - start < 1000000) {
+            count += 1;
+        }
+        let d = now_us() - start;
+        let per_step = d * 1000 / count;
+        io::println(per_step.to_str() + "ns");
+    }
+}
+
